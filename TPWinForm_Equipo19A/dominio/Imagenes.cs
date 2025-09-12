5c85babb54d0e6243f379c31806c4e00f3ad2b8e
jesi <jesileguiza1997@gmail.com>
2025-09-12 13:15:27 -0300

    creacion clases : categoria, marcas e imagenes

---
+++ TPWinForm_Equipo19A/dominio/Imagenes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    internal class Imagenes
+    {
+        public int IdImagen { get; set; }
+
+        public string ImagenUrl { get; set; }
+
+    }
+}
